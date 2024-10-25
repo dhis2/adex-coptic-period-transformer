2c908d8387af68dd73cb56e4da73afbb2eb992be
jason-p-pickering <jason.p.pickering@gmail.com>
2024-10-25 16:17:45 +0200

    Updates

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import re
 
 from flask import Flask, request, jsonify
@@ -16,7 +17,8 @@ logging.basicConfig(level=logging.DEBUG)
 @app.route('/api/dataValueSets', methods=['POST'])
 def process_request():
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config_path = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config.read(config_path)
     base_url = config.get('General', 'base_url')
     # Log the raw incoming request
     app.logger.debug(f'Received request data: {request.data}')
@@ -28,6 +30,9 @@ def process_request():
     except Exception as e:
         app.logger.error(f'Error parsing JSON: {e}')
         return jsonify({'error': 'Invalid JSON format'}), 400
+
+    data['dataValues'] = handle_thirteenth_month(data['dataValues'])
+    data['dataValues'] = handle_storedby(data['dataValues'])
 
     for data_value in data['dataValues']:
         data_value['period'] = convert_coptic_period(data_value['period'])
@@ -61,15 +66,28 @@ def convert_coptic_period(period):
         app.logger.debug(f'Converting yearly period: {period} to {greg_period}')
     elif True:
         app.logger.debug(f'Period {period} is not a valid Coptic period')
-        period = period
-    return period
+        greg_period = period
+    return greg_period
+
+
+def get_last_day_coptic_month(year, month):
+    cal = coptic.monthcalendar(year, month)
+    if len(cal) == 4:
+        if cal[3][0] == 0:
+            last_day = cal[2][0]
+        else:
+            last_day = cal[3][0]
+    else:
+        last_day = cal[3][0]
+    return last_day
 
 
 def convert_coptic_quarter(period):
     coptic_year = int(period[:4])
     coptic_quarter = int(period[-1:])
-    # Get the last day of the coptic quarter
-    gregorian_date = coptic.to_gregorian(coptic_year, coptic_quarter * 3, 30)
+    coptic_month = coptic_quarter * 3
+    last_day = get_last_day_coptic_month(coptic_year, coptic_month)
+    gregorian_date = coptic.to_gregorian(coptic_year, coptic_month, last_day)
     this_day = datetime.datetime(gregorian_date[0], gregorian_date[1], gregorian_date[2])
     quarter = (this_day.month - 1) // 3 + 1
     return f'{this_day.year}Q{quarter}'
@@ -78,15 +96,11 @@ def convert_coptic_quarter(period):
 def convert_coptic_month(period):
     coptic_year = int(period[:4])
     coptic_month = period[-2:]
-    # Put the 13th month as the first month of the next year
-    if coptic_month == '13':
-        coptic_month = '01'
-        coptic_year += 1
-    # Strip the leading zero if present
     if coptic_month[0] == '0':
         coptic_month = coptic_month[1]
     coptic_month = int(coptic_month)
-    gregorian_date = coptic.to_gregorian(coptic_year, coptic_month, 30)
+    last_day = get_last_day_coptic_month(coptic_year, coptic_month)
+    gregorian_date = coptic.to_gregorian(coptic_year, coptic_month, last_day)
     this_day = datetime.datetime(gregorian_date[0], gregorian_date[1], gregorian_date[2])
     return this_day.strftime('%Y%m')
 
@@ -97,6 +111,21 @@ def convert_coptic_year(period):
     gregorian_date = coptic.to_gregorian(coptic_year, 13, 30)
     this_day = datetime.datetime(gregorian_date[0], gregorian_date[1], gregorian_date[2])
     return this_day.strftime('%Y')
+
+
+def handle_thirteenth_month(datavalues):
+    regex = re.compile(r'(\d{4})13')
+    for data_value in datavalues:
+        if regex.match(data_value['period']):
+            datavalues.remove(data_value)
+    return datavalues
+
+
+def handle_storedby(datavalues):
+    for data_value in datavalues:
+        data_value.pop('storedby', None)
+    return datavalues
+
 
 if __name__ == '__main__':
     app.run(debug=True)
